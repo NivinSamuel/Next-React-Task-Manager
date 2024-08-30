f5c335d3d87a0cd7d2f33202488faed0475eb7b1
NivinSamuel <samuelnivin96@gmail.com>
2024-08-30 12:54:03 +0530

    Foundation building

---
+++ app/Components/Sidebar/Sidebar.tsx
@@ -0,0 +1,11 @@
+"use client";
+import styled from "styled-components";
+import React from "react";
+
+function Sidebar(){
+    return <SidebarStyled>Sidebar</SidebarStyled>;
+}
+const SidebarStyled = styled.nav`
+
+`;
+export default Sidebar;
