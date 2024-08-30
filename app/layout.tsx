0cadc05cc94511c2e17d27930df1ddfa5f042700
NivinSamuel <samuelnivin96@gmail.com>
2024-08-30 23:16:28 +0530

    Initial setup and sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,9 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Sidebar from "./Components/Sidebar/Sidebar";
 import "./globals.css";
+import ContextProvider from "./providers/ContextProvider";
 import GlobalStyleProvider from "./providers/GlobalStyleProvider";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,14 +19,27 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+  <ClerkProvider>
     <html lang="en">
+      <head>
+      <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+            integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+            crossOrigin="anonymous"
+            referrerPolicy="no-referrer"
+          />
+      </head>
       <body className={inter.className}>
+        <ContextProvider>
         <GlobalStyleProvider>
             <Sidebar/>
-            {children}
+            <div className="w-full">{children}</div>
         </GlobalStyleProvider>
+        </ContextProvider>
       </body>
 
     </html>
+  </ClerkProvider>
   );
 }
---
+++ app/signin/page.tsx
@@ -0,0 +1,8 @@
+"use client"
+import React from 'react'
+
+function page() {
+  return <div>sign in page</div>
+}
+
+export default page
